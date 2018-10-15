39f15953f82bae6f9a3f7e7e436765f13390b1da
Thomas Kilgour <t.kilgour@gmail.com>
2018-10-15 11:53:05 -0400

    adjust responsive image sizing

--- src/App.vue
+++ src/App.vue
@@ -32,9 +32,10 @@ export default {
       this.photosMetadata = resources;
 
       this.photosMetadata.forEach(photo => {
+        const landscape = photo.width > photo.height;
         this.deanPhotos.push({
           src: `https://res.cloudinary.com/tkilgour/image/upload/h_${this.windowHeight * 2}/v1539392846/${photo.public_id}.${photo.format}`,
-          thumbnail: `https://res.cloudinary.com/tkilgour/image/upload/c_thumb,h_250/v1539392846/${photo.public_id}.${photo.format}`,
+          thumbnail: `https://res.cloudinary.com/tkilgour/image/upload/c_thumb,${landscape ? "h_250" : "w_250"}/v1539392846/${photo.public_id}.${photo.format}`,
           w: photo.width,
           h: photo.height
         });
@@ -45,23 +46,50 @@ export default {
 </script>
 
 <style>
-img[itemprop="thumbnail"] {
+img[src*="w_250"]:not(.pswp__img) {
+  max-width: 125px;
+  margin-top: -10%;
+}
+
+img[src*="h_250"]:not(.pswp__img) {
   max-height: 125px;
+  margin-left: -10%;
+}
+
+figure[itemprop="associatedMedia"] {
+  max-width: 125px;
+  max-height: 125px;
+  overflow: hidden;
 }
 
 @media (max-width: 640px) {
-  img[itemprop="thumbnail"] {
-    max-height: 100px;
+  img[src*="w_250"]:not(.pswp__img) {
+    max-width: 109px;
+  }
+
+  img[src*="h_250"]:not(.pswp__img) {
+    max-height: 109px;
   }
 
   figure[itemprop="associatedMedia"] {
-    margin: 0;
+    margin: 2px;
+    max-width: 109px;
+    max-height: 109px;
   }
 }
 
-@media (max-width: 360px) {
-  img[itemprop="thumbnail"] {
-    max-height: 87px;
+@media (max-width: 374px) {
+  img[src*="w_250"]:not(.pswp__img) {
+    max-width: 90px;
+  }
+
+  img[src*="h_250"]:not(.pswp__img) {
+    max-height: 90px;
+  }
+
+  figure[itemprop="associatedMedia"] {
+    max-width: 90px;
+    max-height: 90px;
   }
 }
 </style>
